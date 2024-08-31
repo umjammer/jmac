9e5ffce63d0eb2c236e1751f45fd65182fc8b361
Naohide Sano <namachapanda2005@gmail.com>
2024-08-31 17:26:53 +0900

    add test case

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright (c) 2024 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.CountDownLatch;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import davaguine.jmac.spi.APEAudioFileReader;
+import davaguine.jmac.spi.APEEncoding;
+import davaguine.jmac.spi.APEFormatConversionProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import vavi.util.Debug;
+import vavi.util.properties.annotation.Property;
+import vavi.util.properties.annotation.PropsEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static vavi.sound.SoundUtil.volume;
+import static vavix.util.DelayedWorker.later;
+
+
+/**
+ * TestCase.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2024/08/29 umjammer initial version <br>
+ */
+@PropsEntity(url = "file:local.properties")
+class TestCase {
+
+    static boolean localPropertiesExists() {
+        return Files.exists(Paths.get("local.properties"));
+    }
+
+    @BeforeEach
+    void setup() throws Exception {
+        if (localPropertiesExists()) {
+            PropsEntity.Util.bind(this);
+        }
+Debug.println("volume: " + volume);
+    }
+
+    static long time = System.getProperty("vavi.test", "").equals("ide") ? 1000 * 1000 : 9 * 1000;
+
+    @Property(name = "vavi.test.volume")
+    double volume = 0.2;
+
+    @Property
+    String ape = "src/test/resources/test.ape";
+
+    @Test
+    @DisplayName("directly")
+    void test0() throws Exception {
+
+        Path path = Paths.get(ape);
+        AudioInputStream sourceAis = new APEAudioFileReader().getAudioInputStream(new BufferedInputStream(Files.newInputStream(path)));
+
+        AudioFormat inAudioFormat = sourceAis.getFormat();
+Debug.println("IN: " + inAudioFormat);
+        AudioFormat outAudioFormat = new AudioFormat(
+                inAudioFormat.getSampleRate(),
+                16,
+                inAudioFormat.getChannels(),
+                true,
+                false);
+Debug.println("OUT: " + outAudioFormat);
+
+        assertTrue(AudioSystem.isConversionSupported(outAudioFormat, inAudioFormat));
+
+        AudioInputStream pcmAis = new APEFormatConversionProvider().getAudioInputStream(outAudioFormat, sourceAis);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, pcmAis.getFormat());
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open(pcmAis.getFormat());
+        line.addLineListener(ev -> Debug.println(ev.getType()));
+        line.start();
+
+        volume(line, volume);
+
+        byte[] buf = new byte[1024];
+        while (!later(time).come()) {
+            int r = pcmAis.read(buf, 0, 1024);
+            if (r < 0) {
+                break;
+            }
+            line.write(buf, 0, r);
+        }
+        line.drain();
+        line.stop();
+        line.close();
+    }
+
+    @Test
+    @DisplayName("directly toritonus")
+    void test01() throws Exception {
+
+        Path path = Paths.get(ape);
+        AudioInputStream sourceAis = new davaguine.jmac.spi.tritonus.APEAudioFileReader().getAudioInputStream(new BufferedInputStream(Files.newInputStream(path)));
+
+        AudioFormat inAudioFormat = sourceAis.getFormat();
+Debug.println("IN: " + inAudioFormat);
+        AudioFormat outAudioFormat = new AudioFormat(
+                inAudioFormat.getSampleRate(),
+                16,
+                inAudioFormat.getChannels(),
+                true,
+                false);
+Debug.println("OUT: " + outAudioFormat);
+
+        assertTrue(AudioSystem.isConversionSupported(outAudioFormat, inAudioFormat));
+
+        AudioInputStream pcmAis = new APEFormatConversionProvider().getAudioInputStream(outAudioFormat, sourceAis);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, pcmAis.getFormat());
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open(pcmAis.getFormat());
+        line.addLineListener(ev -> Debug.println(ev.getType()));
+        line.start();
+
+        volume(line, volume);
+
+        byte[] buf = new byte[1024];
+        while (!later(time).come()) {
+            int r = pcmAis.read(buf, 0, 1024);
+            if (r < 0) {
+                break;
+            }
+            line.write(buf, 0, r);
+        }
+        line.drain();
+        line.stop();
+        line.close();
+    }
+
+    @Test
+    @DisplayName("as spi")
+    void test1() throws Exception {
+
+        Path path = Paths.get(ape);
+        AudioInputStream sourceAis = AudioSystem.getAudioInputStream(new BufferedInputStream(Files.newInputStream(path)));
+
+        AudioFormat inAudioFormat = sourceAis.getFormat();
+Debug.println("IN: " + inAudioFormat);
+        AudioFormat outAudioFormat = new AudioFormat(
+                inAudioFormat.getSampleRate(),
+                16,
+                inAudioFormat.getChannels(),
+                true,
+                false);
+Debug.println("OUT: " + outAudioFormat);
+
+        assertTrue(AudioSystem.isConversionSupported(outAudioFormat, inAudioFormat));
+
+        AudioInputStream pcmAis = AudioSystem.getAudioInputStream(outAudioFormat, sourceAis);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, pcmAis.getFormat());
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open(pcmAis.getFormat());
+        line.addLineListener(ev -> Debug.println(ev.getType()));
+        line.start();
+
+        volume(line, volume);
+
+        byte[] buf = new byte[1024];
+        while (!later(time).come()) {
+            int r = pcmAis.read(buf, 0, 1024);
+            if (r < 0) {
+                break;
+            }
+            line.write(buf, 0, r);
+        }
+        line.drain();
+        line.stop();
+        line.close();
+    }
+
+    @Test
+    @DisplayName("another input type 2")
+    void test2() throws Exception {
+        URL url = Paths.get(ape).toUri().toURL();
+        AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+        assertEquals(APEEncoding.APE, ais.getFormat().getEncoding());
+    }
+
+    @Test
+    @DisplayName("another input type 3")
+    void test3() throws Exception {
+        File file = Paths.get(ape).toFile();
+        AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+        assertEquals(APEEncoding.APE, ais.getFormat().getEncoding());
+    }
+
+    @Test
+    @Disabled("doesn't work well") // TODO
+    @DisplayName("when unsupported file coming")
+    void test5() throws Exception {
+        InputStream is = TestCase.class.getResourceAsStream("/test.caf");
+        int available = is.available();
+        UnsupportedAudioFileException e = assertThrows(UnsupportedAudioFileException.class, () -> AudioSystem.getAudioInputStream(is));
+Debug.println(e.getMessage() + ", " + is);
+Debug.printStackTrace(e);
+        assertEquals(available, is.available()); // spi must not consume input stream even one byte
+    }
+
+    @Test
+    @DisplayName("clip")
+    void test4() throws Exception {
+
+        AudioInputStream ais = AudioSystem.getAudioInputStream(Paths.get(ape).toFile());
+Debug.println(ais.getFormat());
+
+        Clip clip = AudioSystem.getClip();
+        CountDownLatch cdl = new CountDownLatch(1);
+        clip.addLineListener(ev -> {
+Debug.println(ev.getType());
+            if (ev.getType() == LineEvent.Type.STOP)
+                cdl.countDown();
+        });
+        clip.open(AudioSystem.getAudioInputStream(new AudioFormat(44100, 16, 2, true, false), ais));
+        volume(clip, volume);
+        clip.start();
+        if (!System.getProperty("vavi.test", "").equals("ide")) {
+            Thread.sleep(10 * 1000);
+            clip.stop();
+Debug.println("Interrupt");
+        } else {
+            cdl.await();
+        }
+        clip.drain();
+        clip.stop();
+        clip.close();
+    }
+}
